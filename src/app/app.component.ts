c32fd2698f32de0c535d297439bed31ddc4e342b
tmokmss <tomookam@live.jp>
2021-02-08 17:37:24 +0900

    update angular code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { APIService } from './API.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(
+    private apiService: APIService,
+    private platform: Platform
+  ) {
+    this.initializeApp();
+  }
+
+  createTodo() {
+    this.apiService.CreateTodo({
+      name: 'ionic',
+      description: 'testing'
+    });
+  }
+
+  todos: Array<any>;
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      
+      this.apiService.ListTodos().then((evt) => {
+        this.todos = evt.items;
+      });
+
+      this.apiService.OnCreateTodoListener.subscribe((evt) => {
+        const data = (evt as any).value.data.onCreateTodo;
+        this.todos = [...this.todos, data];
+      });
+    });
+  }
 }
